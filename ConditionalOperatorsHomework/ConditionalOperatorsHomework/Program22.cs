056602529f7bc5925b47f3469fc4cb601cbcbd9d
Kolokolova_Vik <2645436@gmail.com>
2024-07-08 19:16:41 +0300

    DZ22
    08.0.2024

---
+++ ConditionalOperatorsHomework/ConditionalOperatorsHomework/Program22.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConditionalOperatorsHomework
+{
+    internal class Program22
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+            Console.WriteLine("Чтобы завершить программу введите 'exit'");
+
+            while (isWork)
+            {
+                string userInput;
+                Console.WriteLine("Введите команду:");
+                userInput = Console.ReadLine();
+
+                if(userInput == "exit")
+                {
+                    isWork = false;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели несуществующую команду!");
+                }
+            }
+
+            Console.WriteLine("Программа завершила своё выполнение!");
+        }
+    }
+}
