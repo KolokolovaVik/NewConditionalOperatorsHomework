e9c847cbb8c3af649d95f66c5a9c4dd0512ead5d
Kolokolova_Vik <2645436@gmail.com>
2024-07-09 13:03:07 +0300

    Homework 2.3
    Commit1

---
+++ ConditionalOperatorsHomework/ConditionalOperatorsHomework/Homework23.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConditionalOperatorsHomework
+{
+    internal class Homework23
+    {
+        static void Main(string[] args)
+        {
+            int maximumNumber;
+
+            Console.WriteLine("Программа выводит последовательность чисел, с прибавлением к последнему числу: 7." +
+                "\nНачальное число последовательности: 5. " +
+                "\nМаскимальный предел последовательности определяете вы сами.");
+            Console.WriteLine("\nВведите максимальное число: ");
+            maximumNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Последовательность: ");
+
+            for (int i = 5; i <= maximumNumber; i += 7)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
--- ConditionalOperatorsHomework/ConditionalOperatorsHomework/Program22.cs
+++ ConditionalOperatorsHomework/ConditionalOperatorsHomework/Program22.cs
@@ -1,33 +1,33 @@
-﻿using System;
+﻿//using System;
 
-namespace ConditionalOperatorsHomework
-{
-    internal class Program22
-    {
-        static void Main(string[] args)
-        {
-            bool isWork = true;
-            string exit = "exit";
+//namespace ConditionalOperatorsHomework
+//{
+//    internal class Program22
+//    {
+//        static void Main(string[] args)
+//        {
+//            bool isWork = true;
+//            string exit = "exit";
 
-            Console.WriteLine($"Чтобы завершить программу введите '{exit}'");
+//            Console.WriteLine($"Чтобы завершить программу введите '{exit}'");
 
-            while (isWork)
-            {
-                string userInput;
-                Console.WriteLine("Введите команду:");
-                userInput = Console.ReadLine();
+//            while (isWork)
+//            {
+//                string userInput;
+//                Console.WriteLine("Введите команду:");
+//                userInput = Console.ReadLine();
 
-                if(userInput == exit)
-                {
-                    isWork = false;
-                }
-                else
-                {
-                    Console.WriteLine("Вы ввели несуществующую команду!");
-                }
-            }
+//                if(userInput == exit)
+//                {
+//                    isWork = false;
+//                }
+//                else
+//                {
+//                    Console.WriteLine("Вы ввели несуществующую команду!");
+//                }
+//            }
 
-            Console.WriteLine("Программа завершила своё выполнение!");
-        }
-    }
-}
+//            Console.WriteLine("Программа завершила своё выполнение!");
+//        }
+//    }
+//}
