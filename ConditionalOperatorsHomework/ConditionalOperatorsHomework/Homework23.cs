dc056e40f89f339edaa58f42b7d7924d50afb304
Kolokolova_Vik <2645436@gmail.com>
2024-07-09 17:05:49 +0300

    Homework24

--- ConditionalOperatorsHomework/ConditionalOperatorsHomework/Homework23.cs
+++ ConditionalOperatorsHomework/ConditionalOperatorsHomework/Homework23.cs
@@ -1,27 +1,27 @@
-﻿using System;
+﻿//using System;
 
-namespace ConditionalOperatorsHomework
-{
-    internal class Homework23
-    {
-        static void Main(string[] args)
-        {
-            int interval = 7;
-            int initialNumber = 5;
+//namespace ConditionalOperatorsHomework
+//{
+//    internal class Homework23
+//    {
+//        static void Main(string[] args)
+//        {
+//            int interval = 7;
+//            int initialNumber = 5;
             
-            int maximumNumber;
+//            int maximumNumber;
 
-            Console.WriteLine($"Программа выводит последовательность чисел, с прибавлением к последнему числу: {interval}." +
-                $"\nНачальное число последовательности: {initialNumber}. " +
-                "\nМаскимальный предел последовательности определяете вы сами.");
-            Console.WriteLine("\nВведите максимальное число: ");
-            maximumNumber = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Последовательность: ");
+//            Console.WriteLine($"Программа выводит последовательность чисел, с прибавлением к последнему числу: {interval}." +
+//                $"\nНачальное число последовательности: {initialNumber}. " +
+//                "\nМаскимальный предел последовательности определяете вы сами.");
+//            Console.WriteLine("\nВведите максимальное число: ");
+//            maximumNumber = Convert.ToInt32(Console.ReadLine());
+//            Console.Write("Последовательность: ");
 
-            for (int i = initialNumber; i <= maximumNumber; i += interval)
-            {
-                Console.Write(i + " ");
-            }
-        }
-    }
-}
+//            for (int i = initialNumber; i <= maximumNumber; i += interval)
+//            {
+//                Console.Write(i + " ");
+//            }
+//        }
+//    }
+//}
--- ConditionalOperatorsHomework/ConditionalOperatorsHomework/Homework24.cs
+++ ConditionalOperatorsHomework/ConditionalOperatorsHomework/Homework24.cs
@@ -1,12 +1,29 @@
-﻿//using System;
+﻿using System;
 
-//namespace ConditionalOperatorsHomework
-//{
-//    internal class Homework24
-//    {
-//        static void Main(string[] args)
-//        {
+namespace ConditionalOperatorsHomework
+{
+    internal class Homework24
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Random random = new Random();
 
-//        }
-//    }
-//}
+            int number;
+
+            number = random.Next(0, 100);
+            Console.WriteLine("Найдём рандомное число не больше 100.");
+            Console.WriteLine($"Рандомное число: {number}.");
+
+            for (int i = 0; i <= number; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine($"\nСумма положительных чисел меньше {number}(включая число), которые кратны 3 или 5 равна: {sum}");
+        }
+    }
+}
