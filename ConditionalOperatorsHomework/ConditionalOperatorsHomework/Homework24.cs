693f5f924bedf7120afe8a00eef0c21d59d4e32d
Kolokolova_Vik <2645436@gmail.com>
2024-07-09 19:11:47 +0300

    Homework25

--- ConditionalOperatorsHomework/ConditionalOperatorsHomework/Homework24.cs
+++ ConditionalOperatorsHomework/ConditionalOperatorsHomework/Homework24.cs
@@ -1,34 +1,34 @@
-﻿using System;
+﻿//using System;
 
-namespace ConditionalOperatorsHomework
-{
-    internal class Homework24
-    {
-        static void Main(string[] args)
-        {
-            int sum = 0;
-            int minRandomNumberRange = 0;
-            int maxRandomNumberRange = 100;
-            int divider1 = 3;
-            int divider2 = 5;
-            Random random = new Random();
+//namespace ConditionalOperatorsHomework
+//{
+//    internal class Homework24
+//    {
+//        static void Main(string[] args)
+//        {
+//            int sum = 0;
+//            int minRandomNumberRange = 0;
+//            int maxRandomNumberRange = 100;
+//            int divider1 = 3;
+//            int divider2 = 5;
+//            Random random = new Random();
 
-            int randomNumber;
+//            int randomNumber;
 
-            randomNumber = random.Next(minRandomNumberRange, maxRandomNumberRange + 1);
-            Console.WriteLine($"Найдём рандомное число не больше {maxRandomNumberRange}.");
-            Console.WriteLine($"Рандомное число: {randomNumber}.");
+//            randomNumber = random.Next(minRandomNumberRange, maxRandomNumberRange + 1);
+//            Console.WriteLine($"Найдём рандомное число не больше {maxRandomNumberRange}.");
+//            Console.WriteLine($"Рандомное число: {randomNumber}.");
 
-            for (int i = 0; i <= randomNumber; i++)
-            {
-                if (i % divider1 == 0 || i % divider2 == 0)
-                {
-                    sum += i;
-                }
-            }
+//            for (int i = 0; i <= randomNumber; i++)
+//            {
+//                if (i % divider1 == 0 || i % divider2 == 0)
+//                {
+//                    sum += i;
+//                }
+//            }
 
-            Console.WriteLine($"\nСумма положительных чисел меньше {randomNumber}(включая число), " +
-                $"которые кратны {divider1} или {divider2} равна: {sum}");
-        }
-    }
-}
+//            Console.WriteLine($"\nСумма положительных чисел меньше {randomNumber}(включая число), " +
+//                $"которые кратны {divider1} или {divider2} равна: {sum}");
+//        }
+//    }
+//}
---
+++ ConditionalOperatorsHomework/ConditionalOperatorsHomework/Homework25.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConditionalOperatorsHomework
+{
+    internal class Homework25
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+            Random random = new Random();
+
+            while (isWork)
+            {
+                string userInput;
+                Console.WriteLine("Добро пожаловать в программу. Введите номер команды:" +
+                    "\n1 - Вывести на экран ваши фамилию и имя." +
+                    "\n2 - Вывести на экран сегодняшнюю дату и время на вашем компьютере." +
+                    "\n3 - Показать случайное число." +
+                    "\n4 - Очистить консоль." +
+                    "\n5 - Выход из программы.");
+
+                userInput = Console.ReadLine();
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine("Как вас зовут?");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Введите вашу фамилию:");
+                        string surname = Console.ReadLine();
+                        Console.WriteLine($"Вас зовут: {name}. Ваша фамилия: {surname}");
+                        break;
+                    case "2":
+                        Console.WriteLine($"Дата и время на компьютере:{DateTime.Now}");
+                        break;
+                    case "3":
+                        Console.WriteLine($"Рандомное число: {random.Next()}");
+                        break;
+                    case "4":
+                        Console.Clear();
+                        break;
+                    case "5":
+                        isWork = false;
+                        break;
+                }
+            }
+        }
+    }
+}
