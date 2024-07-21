be74a336ae735119810f725729751e9690f206c0
Kolokolova_Vik <2645436@gmail.com>
2024-07-21 10:14:40 +0300

    Homework2.8

---
+++ ConditionalOperatorsHomework/ConditionalOperatorsHomework/Homework28.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConditionalOperatorsHomework
+{
+    internal class Homework28
+    {
+        static void Main(string[] args)
+        {
+            int tryCount = 3;
+            string password = "123qwerty";
+
+            while (tryCount > 0)
+            {
+                Console.Write($"Введите пароль для секретного сообщения. У вас есть {tryCount} попытки:");
+                string userInput = Console.ReadLine();
+
+                if (userInput == password)
+                {
+                    Console.WriteLine("Классный сериал: 'Тёмный кристалл: Эпоха сопротивления'");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Пароль неверен. Попробуйте ещё раз!");
+                        tryCount--;
+                }
+            }
+        }
+    }
+}
--- ConditionalOperatorsHomework/ConditionalOperatorsHomework/Homeworl27.cs
+++ ConditionalOperatorsHomework/ConditionalOperatorsHomework/Homeworl27.cs
@@ -1,27 +1,27 @@
-﻿using System;
+﻿//using System;
 
-namespace ConditionalOperatorsHomework
-{
-    internal class Homeworl27
-    {
-        static void Main(string[] args)
-        {
-            string symbolBoundary = "";
+//namespace ConditionalOperatorsHomework
+//{
+//    internal class Homeworl27
+//    {
+//        static void Main(string[] args)
+//        {
+//            string symbolBoundary = "";
 
-            Console.Write("Введите символ для вывода:");
-            string symbol = Console.ReadLine();
-            Console.Write("Введите имя для вывода:");
-            string name = Console.ReadLine();
-            string outputText = symbol + name + symbol;
+//            Console.Write("Введите символ для вывода:");
+//            string symbol = Console.ReadLine();
+//            Console.Write("Введите имя для вывода:");
+//            string name = Console.ReadLine();
+//            string outputText = symbol + name + symbol;
 
-            for (int i = 0; i < outputText.Length; i++)
-            {
-                symbolBoundary += symbol;
-            }
+//            for (int i = 0; i < outputText.Length; i++)
+//            {
+//                symbolBoundary += symbol;
+//            }
 
-            Console.WriteLine(symbolBoundary);
-            Console.WriteLine(outputText);
-            Console.WriteLine(symbolBoundary);
-        }
-    }
-}
+//            Console.WriteLine(symbolBoundary);
+//            Console.WriteLine(outputText);
+//            Console.WriteLine(symbolBoundary);
+//        }
+//    }
+//}
