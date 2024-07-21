2b5c3b0519c6b014a365453509bae5928e05fe92
Kolokolova_Vik <2645436@gmail.com>
2024-07-21 20:44:14 +0300

    Homework 2.8 FOR

--- ConditionalOperatorsHomework/ConditionalOperatorsHomework/Homework28.cs
+++ ConditionalOperatorsHomework/ConditionalOperatorsHomework/Homework28.cs
@@ -1,30 +1,29 @@
-﻿//using System;
+﻿using System;
 
-//namespace ConditionalOperatorsHomework
-//{
-//    internal class Homework28
-//    {
-//        static void Main(string[] args)
-//        {
-//            int tryCount = 3;
-//            string password = "123qwerty";
+namespace ConditionalOperatorsHomework
+{
+    internal class Homework28
+    {
+        static void Main(string[] args)
+        {
+            int tryCount = 3;
+            string password = "123qwerty";
 
-//            while (tryCount > 0)
-//            {
-//                Console.Write($"Введите пароль для секретного сообщения. У вас есть {tryCount} попытки:");
-//                string userInput = Console.ReadLine();
+            for (int i = tryCount; i > 0; i--)
+            {
+                Console.Write($"Введите пароль для секретного сообщения. У вас есть {i} попытки:");
+                string userInput = Console.ReadLine();
 
-//                if (userInput == password)
-//                {
-//                    Console.WriteLine("Классный сериал: 'Тёмный кристалл: Эпоха сопротивления'");
-//                    break;
-//                }
-//                else
-//                {
-//                    Console.WriteLine("Пароль неверен. Попробуйте ещё раз!");
-//                    tryCount--;
-//                }
-//            }
-//        }
-//    }
-//}
+                if (userInput == password)
+                {
+                    Console.WriteLine("Классный сериал: 'Тёмный кристалл: Эпоха сопротивления'");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Пароль неверен. Попробуйте ещё раз!");
+                }
+            }
+        }
+    }
+}
--- ConditionalOperatorsHomework/ConditionalOperatorsHomework/Homework29.cs
+++ ConditionalOperatorsHomework/ConditionalOperatorsHomework/Homework29.cs
@@ -1,30 +1,30 @@
-﻿using System;
+﻿//using System;
 
-namespace ConditionalOperatorsHomework
-{
-    internal class Homework29
-    {
-        static void Main(string[] args)
-        {
-            int minNumber = 50;
-            int maximumNumber = 150;
-            int countNumber = 0;
-            int minNumberRandom = 10;
-            int maxNumberRandom = 26;
+//namespace ConditionalOperatorsHomework
+//{
+//    internal class Homework29
+//    {
+//        static void Main(string[] args)
+//        {
+//            int minNumber = 50;
+//            int maximumNumber = 150;
+//            int countNumber = 0;
+//            int minNumberRandom = 10;
+//            int maxNumberRandom = 26;
 
-            Random random = new Random();
-            int numberN = random.Next(minNumberRandom, maxNumberRandom);
-            Console.WriteLine($"Рандомное число: {numberN}");
+//            Random random = new Random();
+//            int numberN = random.Next(minNumberRandom, maxNumberRandom);
+//            Console.WriteLine($"Рандомное число: {numberN}");
 
-            for (int i = numberN; i <= maximumNumber; i += numberN)
-            {
-                if (minNumber <= i && i <= maximumNumber)
-                {
-                    countNumber += 1;
-                }
-            }
-            Console.WriteLine($"Числа кратные {numberN}, которые больше {minNumber}" +
-                $" и меньше {maximumNumber} в количестве составляют: {countNumber} ");
-        }
-    }
-}
+//            for (int i = numberN; i <= maximumNumber; i += numberN)
+//            {
+//                if (minNumber <= i && i <= maximumNumber)
+//                {
+//                    countNumber += 1;
+//                }
+//            }
+//            Console.WriteLine($"Числа кратные {numberN}, которые больше {minNumber}" +
+//                $" и меньше {maximumNumber} в количестве составляют: {countNumber} ");
+//        }
+//    }
+//}
