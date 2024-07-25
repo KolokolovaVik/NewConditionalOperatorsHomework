4ae4872d787c3471c79ab015ee6cca413f432670
Kolokolova_Vik <2645436@gmail.com>
2024-07-25 10:07:24 +0300

    Homework 2. 11 Corrections

--- ConditionalOperatorsHomework/ConditionalOperatorsHomework/Homework211.cs
+++ ConditionalOperatorsHomework/ConditionalOperatorsHomework/Homework211.cs
@@ -11,108 +11,110 @@ namespace ConditionalOperatorsHomework
             const string CommandExplosionAttackHero = "3";
             const string CommandTreatmentHero = "4";
 
+            int minAttackBoss = 10;
+            int maxAttackBoss = 45;
+            int minAttackHero = 25;
+            int maxAttackHero = 35;
+            int minFireballAttackHero = 10;
+            int maxFireballAttackHero = 21;
+            int minExplosionAttackHero = 20;
+            int maxExplosionAttackHero = 26;
+
             Random random = new Random();
             int healthBoss = 100;
             int healthHero = 100;
-            int attackBoss = random.Next(10, 45);
-            int attackHero = random.Next(25, 35);
-            int fireballAttackHero = random.Next(10, 21);
-            int explosionAttackHero = random.Next(20, 26);
-            bool isBattle = true;
+            int attackBoss = random.Next(minAttackBoss, maxAttackBoss);
+            int attackHero = random.Next(minAttackHero, maxAttackHero);
+            int fireballAttackHero = random.Next(minFireballAttackHero, maxFireballAttackHero);
+            int explosionAttackHero = random.Next(minExplosionAttackHero, maxExplosionAttackHero);
+            int countMana = fireballAttackHero;
 
             int initialHealth = healthHero;
-            int initialMana = fireballAttackHero;
+            int initialMana = countMana;
             bool isFireballAttack = false;
             int countRecoveries = 2;
 
+            string userInput;
+
             Console.WriteLine($"Здоровье героя: {healthHero}. Обычный урон: {attackHero}. " +
-                       $"Урон огненным шаром: {fireballAttackHero}. " +
+                       $"Урон огненным шаром: {fireballAttackHero}. Мана: {countMana} " +
                        $"Урон взрывом: {explosionAttackHero}");
             Console.WriteLine($"Здоровье босса: {healthBoss}. Урон от атаки босса: {attackBoss}");
 
-            while (isBattle)
+            while (healthBoss > 0 && healthHero > 0)
             {
-                string userInput;
+                Console.WriteLine($"\nВыберите, какую способность использовать герою:" +
+                    $"\n{CommandAttackHero} - Нанести обычную атаку." +
+                    $"\n{CommandFireballAttackHero} - Нанести атаку огненным шаром (тратит ману). " +
+                    $"Способность можно использовать только, когда у героя есть мана." +
+                    $"\n{CommandExplosionAttackHero} - Нанести урон взрывом. Может быть нанесён только после использования огненного шара, каждый раз." +
+                    $"\n{CommandTreatmentHero} - Лечить здоровье и ману. Осталось {countRecoveries} лечение.");
 
-                if (healthBoss > 0 && healthHero > 0)
+                userInput = Console.ReadLine();
+
+                switch (userInput)
                 {
-                    healthHero -= attackBoss;
-                    Console.WriteLine("\nБосс нанёс удар герою.");
+                    case CommandAttackHero:
+                        Console.WriteLine("\nСовершена обычная атака на босса.");
+                        healthBoss -= attackHero;
+                        break;
 
-                    Console.WriteLine($"\nВыберите, какую способность использовать герою:" +
-                        $"\n{CommandAttackHero} - нанести обычную атаку." +
-                        $"\n{CommandFireballAttackHero} - нанести атаку огненным шаром (тратит ману). " +
-                        $"Способность можно использовать только, когда у героя есть сила." +
-                        $"\n{CommandExplosionAttackHero} - нанести урон взрывом. Может быть нанесён только после использования огненного шара, каждый раз." +
-                        $"\n{CommandTreatmentHero} - Лечить здоровье и ману. Осталось {countRecoveries} лечение.");
+                    case CommandFireballAttackHero:
+                        if (countMana > 0)
+                        {
+                            Console.WriteLine("\nСовершена атака на босса огненным шаром.");
+                            healthBoss -= fireballAttackHero;
+                            countMana -= initialMana;
+                            isFireballAttack = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nНедостаточно маны для удара огненным шаром.");
+                        }
+                        break;
 
-                    userInput = Console.ReadLine();
+                    case CommandExplosionAttackHero:
+                        if (isFireballAttack == true)
+                        {
+                            Console.WriteLine("\nСовершена атака на босса взрывом.");
+                            healthBoss -= explosionAttackHero;
+                            isFireballAttack = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nВзрыв можно сделать только после огненного шара.");
+                        }
+                        break;
 
-                    switch (userInput)
-                    {
-                        case CommandAttackHero:
-                            Console.WriteLine("\nСовершена обычная атака на босса.");
-                            healthBoss -= attackHero;
-                            break;
+                    case CommandTreatmentHero:
+                        if (countRecoveries > 0)
+                        {
+                            Console.WriteLine("\nГерой восстановил здоровье и ману.");
+                            int template = initialHealth - healthHero;
+                            healthHero += template;
 
-                        case CommandFireballAttackHero:
-                            if (fireballAttackHero > 0)
-                            {
-                                Console.WriteLine("\nСовершена атака на босса огненным шаром.");
-                                healthBoss -= fireballAttackHero;
-                                fireballAttackHero -= fireballAttackHero;
-                                isFireballAttack = true;
-                            }
-                            else
-                            {
-                                Console.WriteLine("\nНедостаточно маны для удара огненным шаром.");
-                            }
-                            break;
+                            template = initialMana - countMana;
+                            countMana += template;
+                            countRecoveries--;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Герой исчерплал лимит лечения. Дерись сам!");
+                        }
+                        break;
 
-                        case CommandExplosionAttackHero:
-                            if (isFireballAttack == true)
-                            {
-                                Console.WriteLine("\nСовершена атака на босса взрывом.");
-                                healthBoss -= explosionAttackHero;
-                                isFireballAttack = false;
-                            }
-                            else
-                            {
-                                Console.WriteLine("\nВзрыв можно сделать только после огненного шара.");
-                            }
-                            break;
-
-                        case CommandTreatmentHero:
-                            if (countRecoveries > 0)
-                            {
-                                Console.WriteLine("\nГерой восстановил здоровье и ману.");
-                                int template = initialHealth - healthHero;
-                                healthHero += template;
-
-                                template = initialMana - fireballAttackHero;
-                                fireballAttackHero += template;
-                                countRecoveries--;
-                            }
-                            else
-                            {
-                                Console.WriteLine("Герой исчерплал лимит лечения. Дерись сам!");
-                            }
-                            break;
-
-                        default:
-                            Console.WriteLine("Такой способности не сущестует.");
-                            break;
-                    }
-
-                    Console.WriteLine($"Здоровье героя: {healthHero}. Обычный урон: {attackHero}. " +
-                       $"Урон огненным шаром: {fireballAttackHero}. " +
-                       $"Урон взрывом: {explosionAttackHero}");
-                    Console.WriteLine($"Здоровье босса: {healthBoss}. Урон от атаки босса: {attackBoss}");
+                    default:
+                        Console.WriteLine("Такой способности не сущестует.");
+                        break;
                 }
-                else
-                {
-                    isBattle = false;
-                }
+
+                healthHero -= attackBoss;
+                Console.WriteLine("Босс нанёс удар герою.");
+
+                Console.WriteLine($"\nЗдоровье героя: {healthHero}. Обычный урон: {attackHero}. " +
+                   $"Урон огненным шаром: {fireballAttackHero}. Мана {countMana}. " +
+                   $"Урон взрывом: {explosionAttackHero}");
+                Console.WriteLine($"Здоровье босса: {healthBoss}. Урон от атаки босса: {attackBoss}");
             }
 
             if (healthBoss <= 0 && healthHero <= 0)
@@ -123,7 +125,7 @@ namespace ConditionalOperatorsHomework
             {
                 Console.WriteLine($"Босс погиб!");
             }
-            else if (healthHero <= 0)
+            else
             {
                 Console.WriteLine("Герой погиб!");
             }
